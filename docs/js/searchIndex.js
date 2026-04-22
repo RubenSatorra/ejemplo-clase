44fdf7568e8858e8875092414f7f93f76ae3dc8f
ruben <reciomatamoros13@gmail.com>
2026-04-22 19:32:10 +0200

    actualizada la carpeta de documentacion

--- docs/js/searchIndex.js
+++ docs/js/searchIndex.js
@@ -1,14 +1,4 @@
 Search.appendIndex(
     [
-                {
-            "fqsen": "\\ejemplo\u0028\u0029",
-            "name": "ejemplo",
-            "summary": "",
-            "url": "namespaces/default.html#function_ejemplo"
-        },                {
-            "fqsen": "\\",
-            "name": "\\",
-            "summary": "",
-            "url": "namespaces/default.html"
-        }            ]
+            ]
 );
